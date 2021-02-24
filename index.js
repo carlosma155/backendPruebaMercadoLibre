ccbf33af6257feafd340f4b93ee925cdd1e81726
Carlos Benitez <carlosma155@hotmail.com>
2021-02-23 20:57:35 -0500

    Se hizo la configuracion para desplegar la API en la plataforma vercel

--- index.js
+++ index.js
@@ -4,15 +4,6 @@ const app = express();
 const { config } = require('./config/index');
 const itemsApi = require('./routes/items.js');
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
-
-
 itemsApi(app);
 
 app.listen(config.port, () => {
